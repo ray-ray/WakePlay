73909cce1099c0bd84a53d91ad36a9dbfec8cea3
RAY <ray@jawbone.com>
2014-07-31 18:39:55 -0700

    added local data cache and status, test, and play routes

--- WakePlay.py
+++ WakePlay.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 
 
+import data
 import json
 
 app = Flask(__name__)
@@ -21,6 +22,34 @@ def wakeup():
     else:
         return "Here's the pubsub!"
 
+
+def status_str():
+    output = "<h1>Who's Awake?</h1><table><tr><th>Name</th><th>State</th></tr>"
+    for user in data.users.itervalues():
+        output += "<tr><td>%s</td><td>%s</td></tr>" % (user['name'], user['state'])
+    output += '</table>'
+    return output
+
+
+@app.route('/status')
+def status():
+    return status_str()
+
+@app.route('/test')
+def test():
+    if data.users['cMqObMnzHc8']['state'] == 'awake':
+        data.users['cMqObMnzHc8']['state'] = 'asleep'
+    else:
+        data.users['cMqObMnzHc8']['state'] = 'awake'
+    return status_str()
+
+
+@app.route('/play')
+def play():
+    """
+    Add logic here for the pandora redirects based on sleep state.
+    """
+    return 'Not done yet'
 
 if __name__ == '__main__':
     app.run()
---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'rcourtney'
+
+users = {
+    'cMqObMnzHc8': {'name': 'RAY', 'state': 'awake'},
+    'CC': {'name': 'CC', 'state': 'awake'}
+}
+
