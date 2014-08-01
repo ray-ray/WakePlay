f3d8033d4fdb02cf10c04b53f9029fe4695b9f3e
RAY <ray@jawbone.com>
2014-07-31 17:36:43 -0700

    simple pubsub endpoint

--- WakePlay.py
+++ WakePlay.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/wakeup')
+def wakeup():
+    return "Here's the pubsub!"
 
 
 if __name__ == '__main__':
